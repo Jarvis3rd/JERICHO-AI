841e0fb8d1463560100022733c593392546c5c7e
Jarvis3rd <secretlloyd@gmail.com>
2026-03-10 21:13:38 -0700

    Change voice parameter from 'onyx' to 'ash'

--- agent.py
+++ agent.py
@@ -29,7 +29,7 @@ class Assistant(Agent):
             instructions=AGENT_INSTRUCTION,
             llm=openai.realtime.RealtimeModel(
                 model="gpt-4o-realtime-preview",
-                voice="onyx",
+                voice="ash"
                 temperature=0.6,
                 turn_detection=TurnDetection(
                     type="server_vad",
